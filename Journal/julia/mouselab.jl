7e1bf0e89cbde3760499f8fe18f62e13f4136007
fredcallaway <fredc@llaway.com>
2018-11-11 21:50:12 -0500

    Mouselab seems to be all working, with 120x speedup over Python!

--- Journal/julia/mouselab.jl
+++ Journal/julia/mouselab.jl
@@ -3,11 +3,12 @@ using Distributions
 import Base
 using Memoize
 
-const TERM = 0
-
+const TERM = 0  # termination action
+const NULL_FEATURES = -1e10 * ones(4)  # features for illegal computation
 
 # =================== Problem =================== #
 
+"Parameters defining a class of mouselab problems."
 @with_kw struct Params
     n_gamble::Int = 2
     n_attr::Int = 3
@@ -16,6 +17,7 @@ const TERM = 0
     cost::Float64 = 0.1
 end
 
+"An individual mouselab problem (with sampled values)"
 struct Problem
     prm::Params
     matrix::Matrix{Float64}
@@ -30,14 +32,22 @@ Problem(prm::Params) = begin
         rand(Dirichlet(dispersion * ones(n_attr)))
     )
 end
-payoff(matrix, weights, choice) = sum((weights .* matrix)[:, choice])
-payoff(prob::Problem, choice) = payoff(prob.matrix, prob.weights, choice)
 
+"Value of each gamble according to the true cell values OR a belief.
+    If x is a Belief, a vector of distributions is returned.
+    If x is a Problem, a vector a floats is returned.
+"
+gamble_values(x) = sum(x.weights .* x.matrix, dims=1)
+
+"A belief about the values for a Problem.
+    Currently, weights and cost are assumed to be known exactly.
+"
 struct Belief
     matrix::Matrix{Normal}
     weights::Vector{Float64}
     cost::Float64
 end
+"Initial belief for a given problem."
 Belief(p::Problem) = begin
     Belief(
         [p.prm.reward_dist for i in 1:p.prm.n_attr, j in 1:p.prm.n_gamble],
@@ -52,55 +62,78 @@ Base.display(b::Belief) = begin
     display(X)
 end
 
-U(b::Belief) = maximum(b.weights' * mean.(b.matrix))
+"Expected value of terminating computation with a given belief."
+term_reward(b::Belief) = maximum(b.weights' * mean.(b.matrix))
 
+"Update a belief by observing the true value of a cell."
 function observe!(b::Belief, p::Problem, i::Int)
     b.matrix[i] = Normal(p.matrix[i], 1e-20)
+end
+"Update a belief by sampling the value of a cell."
+function observe!(b::Belief, i::Int)
+    val = rand(b.matrix[i])
+    # Belief.matrix contains only Normals, so we represent certainty
+    # with an extremeley low variance.
+    b.matrix[i] = Normal(val, 1e-20)
 end
 observed(b::Belief, cell::Int) = b.matrix[cell].σ == 1e-20
 
 
 # =================== Features =================== #
 
-@memoize samples(d, g, a; n=10000) = rand(d, n)
-function voi_map(b::Belief, obs::BitArray{2})
+"Define basic arithmetic operations on Normal distributions."
+Base.:*(x::Number, n::Normal) = Normal(x * n.µ, x * n.σ)
+Base.:+(a::Normal, b::Normal) = Normal(a.μ + b.μ, √(a.σ^2 + b.σ^2))
+Base.:+(a::Normal, x::Number) = Normal(a.μ + x, a.σ)
+Base.:+(x::Number, a::Normal) = Normal(a.μ + x, a.σ)
+Base.zero(x::Normal) = Normal(0, 1e-20)
+
+"Highest value in x not including x[c]"
+function competing_value(x::Vector{Float64}, c::Int)
+    tmp = x[c]
+    x[c] = -Inf
+    val = maximum(x)
+    x[c] = tmp
+    val
+end
+
+"Expected maximum of a normal and a number."
+function emax(d::Normal, c::Float64)
+    p_improve = 1 - cdf(d, c)
+    (1 - p_improve)  * c + p_improve * mean(Truncated(d, c, Inf))
+end
+emax(x::Float64, c::Float64) = max(x, c)
+
+"Value of knowing the true value of a gamble."
+function voi_gamble(b::Belief, item::Int)
+    item_dists = item_values(b)
+    μ = mean.(item_dists)[:]
+    cv = competing_value(µ, item)
+    emax(item_dists[item], cv) - maximum(μ)
+end
+
+"Value of knowing the value in a cell."
+function voi1(b::Belief, cell::Int)
     n_attr, n_gamble = size(b.matrix)
-    sample_cell(g, a) = begin
-        d = b.matrix[a, g]
-        obs[a, g] & (d.σ > 1e-10) ? samples(d, g, a) : [d.μ]
-    end
-    w = b.weights
-    sample_gamble(g) = .+((w[a] * sample_cell(g, a) for a in 1:n_attr)...)
-    v = mean(max.((sample_gamble(g) for g in 1:n_gamble)...))
-    v - U(b)
+    item = Int(ceil(cell / n_attr))
+    attr = cell % n_attr
+    col = b.matrix[:, item]
+    new_dist = sum(b.weights[i] * (i == attr ? d : d.μ)
+                   for (i, d) in enumerate(col))
+    item_dists = item_values(b)
+    μ = mean.(item_dists)[:]
+    cv = competing_value(µ, item)
+    emax(new_dist, cv) - maximum(μ)
 end
 
-function vpi(b)
-    obs = trues(size(b.matrix))
-    voi_map(b, obs)
+"Value of knowing everything."
+function vpi(b::Belief)
+    item_dists = item_values(b)
+    μ = mean.(item_dists)[:]
+    mean(max.((rand(d, n_sample) for d in item_dists)...)) - maximum(μ)
 end
 
-function voi1(b, cell)
-    obs = falses(size(b.matrix))
-    obs[cell] = true
-    voi_map(b, obs)
-end
-
-function voi_gamble(b, gamble)
-    obs = falses(size(b.matrix))
-    obs[:, gamble] .= true
-    voi_map(b, obs)
-end
-
-function time_features()
-    println("-"^70)
-    @time voi1(Belief(p), 1); nothing
-    @time voi_gamble(b, 1)
-    @time vpi(Belief(p)); nothing
-    @time features(p, b); nothing
-end
-
-const NULL_FEATURES = -1e10 * ones(4)
+"Features for every computation in a given belief."
 function features(b::Belief)
     n_attr, n_gamble = size(b.matrix)
     vpi_b = vpi(b)
@@ -114,39 +147,35 @@ function features(b::Belief)
     hcat((phi(a) for a in eachindex(b.matrix))...)
 end
 
-function test_features()
-    p = Problem(Params())
-    b = Belief(p)
-    for i in eachindex(b.matrix)
-        observe!(b, p, i)
-    end
-    display(features(p, b))
-    bmps_policy(Float64[1,1,1,1])(p, b)
-end
-
-
 # ========== Policy ========== #
-
-function bmps_policy(θ::Array{Float64})
-    function π(b)
-        voc = (θ' * features(b))' .- b.cost
-        v, c = findmax(voc)
-        v <= 0 ? TERM : c
-    end
+"A metalevel policy that uses the BMPS features"
+struct Policy
+    θ::Vector{Float64}
+end
+"Selects a computation to perform in a given belief.
+    e.g. Policy(θ)(b) -> c
+"
+(π::Policy)(b::Belief) = begin
+    voc = (π.θ' * features(b))' .- b.cost
+    v, c = findmax(voc)
+    v <= 0 ? TERM : c
 end
 
-function rollout(p::Problem, π; max_steps=100)
+"Runs a Policy on a Problem."
+function rollout(p::Problem, π::Policy; max_steps=100, belief_log=nothing)
     b = Belief(p)
     reward = 0
     for step in 1:max_steps
-        a = (step == max_steps) ? TERM : π(b)
-        if a == TERM
-            reward += U(b)
-            return (belief=b, reward=reward, steps=step)
+        if belief_log != nothing
+            push!(belief_log, copy(b.matrix))
+        end
+        c = (step == max_steps) ? TERM : π(b)
+        if c == TERM
+            reward += term_reward(b)
+            return (belief=b, reward=reward, n_steps=step)
         else
             reward -= p.prm.cost
-            observe!(b, p, a)
+            observe!(b, p, c)
         end
-
     end
 end
--- Journal/julia/test_mouselab.jl
+++ Journal/julia/test_mouselab.jl
@@ -1,21 +1,45 @@
 include("mouselab.jl")
 
-prm = Params()
-p = Problem(prm)
-b = Belief(p)
-display(b.weights)
 
-pol = bmps_policy([0., 1, 0, 0])
-w = [0.75, 0.25, 0]
-b.weights[:] = w
-voi1_ = reshape(features(b)[2, :], size(b.matrix))
-@assert sum(abs.(voi1_[:, 1] .- voi1_[:, 2])) < 0.01
-@assert sum(abs.((voi1_[1, :] ./ voi1_[2, :]) .- w[1] / w[2])) < 0.1
-@assert sum(voi1_[3, :]) == 0
+# prm = Params()
+# p = Problem(prm)
+# b = Belief(p)
+# display(b.weights)
+# pol = bmps_policy([0., 1, 0, 0])
+# w = [0.75, 0.25, 0]
+# b.weights[:] = w
+# voi1_ = reshape(features(b)[2, :], size(b.matrix))
+# @assert sum(abs.(voi1_[:, 1] .- voi1_[:, 2])) < 0.01
+# @assert sum(abs.((voi1_[1, :] ./ voi1_[2, :]) .- w[1] / w[2])) < 0.1
+# @assert sum(voi1_[3, :]) == 0
 
 
 prm = Params(n_gamble=7, n_attr=4, cost=0.1)
 pol = bmps_policy([0., 0, 0, 1])
 p = Problem(prm)
 b = Belief(p)
-@time rollout(p, pol).steps
+# @profiler rollout(p, pol).n_steps
+
+function time_features()
+    b = Belief(p)
+    println("-"^30, " Timing Features ", "-"^30)
+    print("voi1       "); @time voi1(b, 1); nothing
+    print("voi_gamble "); @time voi_gamble(b, 1)
+    print("vpi        "); @time vpi(b); nothing
+    print("features   "); @time features(b); nothing
+end
+
+time_features()
+
+function test_features()
+    p = Problem(Params())
+    b = Belief(p)
+    for i in eachindex(b.matrix)
+        observe!(b, p, i)
+    end
+    display(features(p, b))
+    bmps_policy(Float64[1,1,1,1])(p, b)
+end
+
+pol = Policy([0., 0, 0, 1])
+print("rollout time "); @time rollout(p, pol).n_steps; nothing
